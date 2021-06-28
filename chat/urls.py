47cbc898746bd5bb2589f97a7a0e9472b53520fd
swati Jindal <swatijindal2018@gmail.com>
2021-06-28 11:28:20 +0530

    commit

--- chat/urls.py
+++ chat/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import main_view
+from .views import main_view,home
 
 urlpatterns = [
     path('', main_view, name='main_view'),
+    path('home',home, name='home'),
+    
 ]
--- chat/views.py
+++ chat/views.py
@@ -6,3 +6,10 @@ def main_view(request):
     context = {}
 
     return render(request,'chat/main.html',context=context)
+
+def home(request):
+    context={}
+
+    return render(request,'chat/base.html',context=context)
+
+
